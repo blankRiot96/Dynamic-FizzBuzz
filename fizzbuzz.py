d96e3f6c90c4801ed05a95af679454593bc6212b
Axis <77634274+blankRiot96@users.noreply.github.com>
2021-08-06 20:12:49 +0530

    Create fizzbuzz.py

---
+++ fizzbuzz.py
@@ -0,0 +1,17 @@
+combos = {
+3: "Fizz",
+5: "Buzz",
+7: "Oniichan UnGH"
+}
+
+output = ""
+for i in range(100+1):
+    for special in combos:
+        if i % special == 0:
+            output += combos[special]
+    
+    if output == "":
+        output += str(i)
+     
+    print(output)
+    output = ""
